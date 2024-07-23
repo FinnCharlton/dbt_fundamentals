b039db2f5b7c1b0b277115de3c0191c450102fa0
FinnCharlton <finn.charlton@hotmail.com>
2024-07-23 16:40:05 +0100

    commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -6,22 +5,24 @@
 
     Try changing "table" to "view" below
 */
+{{ config(
+    materialized = 'view'
+) }}
 
-{{ config(materialized='table') }}
+WITH source_data AS (
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
+    SELECT
+        1 AS id
+    UNION ALL
+    SELECT
+        NULL AS id
 )
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+SELECT
+    *
+FROM
+    source_data
+    /*
+        Uncomment the line below to remove records with null `id` values
+    */
+WHERE
+    id IS NOT NULL
